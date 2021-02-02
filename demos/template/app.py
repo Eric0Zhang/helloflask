122dc2c97c4e82325102c9352e7051f92f01400f
Eric0Zhang <silentstorm321f@qq.com>
2021-02-02 23:03:21 +0800

    模板学习注释更新

--- demos/template/app.py
+++ demos/template/app.py
@@ -9,11 +9,11 @@ import os
 from flask import Flask, render_template, flash, redirect, url_for, Markup
 
 app = Flask(__name__)
-# 获取密码环境变量, 默认secret string
+# 获取密码环境变量, 默认secret string 如果不设置密码 flash在调用session时报错: The session is unavailable because no secret key was set.
 app.secret_key = os.getenv("SECRET_KEY", "secret string")
-# jinja在等人环境变量
-app.jinja_env.trim_blocks = True # If this is set to True the first newline after a block is removed (block, not variable tag!). Defaults to False.
-app.jinja_env.lstrip_blocks = True # If this is set to True leading spaces and tabs are stripped from the start of a line to a block. Defaults to False.
+# jinja的环境变量, 这两个用来去除block的空白行
+app.jinja_env.trim_blocks = True  # If this is set to True the first newline after a block is removed (block, not variable tag!). Defaults to False.
+app.jinja_env.lstrip_blocks = True  # If this is set to True leading spaces and tabs are stripped from the start of a line to a block. Defaults to False.
 
 user = {
     "username": "Grey Li",
@@ -79,7 +79,7 @@ def watchlist_with_static():
 # message flashing
 @app.route("/flash")
 def just_flash():
-    flash("I am flash, who is looking for me?")
+    flash("哈哈哈 Flash消息")  # 可以通过get_flashed_messages()获得消息
     return redirect(url_for("index"))
 
 
